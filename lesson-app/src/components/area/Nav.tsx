60efcfc183685a7b25329a0b32107cfaa043e531
jp-knj <kenji.tomita1996@gmail.com>
2021-09-11 20:39:24 +0900

    :art: create mobile nav

--- lesson-app/src/components/area/Nav.tsx
+++ lesson-app/src/components/area/Nav.tsx
@@ -1,4 +1,30 @@
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import "./Nav.css";
+import { useWindowDimensions } from "../../hooks/useWindowDimensions";
+// import ReactModal from "react-modal";
+// import SideBarMenus from "./sidebar/SideBarMenus";
+
 const Nav = () => {
-    return <nav className="navigation">Nav</nav>
+    const { width } = useWindowDimensions()
+    const getMobileMenu = () => {
+        if (width <= 768) {
+            return (
+                <FontAwesomeIcon
+                    icon={faBars}
+                    size="lg"
+                    className="nav-mobile-menu"
+                />
+            );
+        }
+        return null;
+    };
+    return (
+        <nav className="navigation">
+            {getMobileMenu()}
+            <strong>SuperForum</strong>
+        </nav>
+    )
 }
 export default Nav
