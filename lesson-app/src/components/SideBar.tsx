15dda9ffcddaaaa35dec6c94fbba23ebe0e34dd1
jp-knj <kenji.tomita1996@gmail.com>
2021-09-11 14:42:20 +0900

    :sparkles: add icon

--- lesson-app/src/components/SideBar.tsx
+++
@@ -1,9 +0,0 @@
-import {useWindowDimensions} from "../hooks/useWindowDimensions"
-const SideBar = () => {
-    const { width } = useWindowDimensions()
-    if(width <= 768){
-        return null
-    }
-    return <div className="sidebar">sidebar</div>
-}
-export default SideBar
