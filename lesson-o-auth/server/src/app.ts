00d1a2016c0127d096ad74ed75b4d98ffaae83d9
jp-knj <kenji.tomita1996@gmail.com>
2021-09-11 16:43:33 +0900

    :tada: get url

--- lesson-o-auth/server/src/app.ts
+++ lesson-o-auth/server/src/app.ts
@@ -1,7 +1,14 @@
 import express, { Request, Response} from "express";
-
+import { get} from "lodash"
 const app = express();
 
+app.get('/api/auth/github', (req: Request, res: Response) => {
+    const code = get(req, "query.code")
+    const path = get(req, "query.path", "/")
+    if(!code) {
+        throw new Error("No Code");
+    }
+})
 app.listen(4000, () => {
     console.log("Server is listening");
 })
