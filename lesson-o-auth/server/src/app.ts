4ef3f54e54b6b12d261e463e276cdf5f14b2519e
jp-knj <kenji.tomita1996@gmail.com>
2021-09-11 17:02:17 +0900

    :tada: create getGithubUser

--- lesson-o-auth/server/src/app.ts
+++ lesson-o-auth/server/src/app.ts
@@ -1,13 +1,47 @@
 import express, { Request, Response} from "express";
+import querystring from "querystring"
+import axios from "axios"
 import { get} from "lodash"
 const app = express();
 
-app.get('/api/auth/github', (req: Request, res: Response) => {
+const GITHUB_CLIENT_ID = "f3f6b47cb120839f2e9c";
+const GITHUB_CLIENT_SECRET = "300ca8013b5eadb65db6bb0da5d2af5d4499ea88";
+
+async function getGithubUser({code}: {code: string}){
+    const githubToken = await axios
+        .post(
+            `https://github.com/login/oauth/access_token?client_id=${GITHUB_CLIENT_ID}&client_secret=${GITHUB_CLIENT_SECRET}&code=${code}`
+        )
+        .then((res) => res.data)
+
+        .catch((error) => {
+            throw error;
+        });
+
+    const decoded = querystring.parse(githubToken)
+    const accessToken = decoded.access_token;
+
+    return axios
+        .get("https://api.github.com/user", {
+            headers: {Authorization: `${accessToken}`}
+        })
+        .then((res) => res.data)
+        .catch((error) => {
+            console.log(`Error getting user from Github`);
+            throw error
+        })
+}
+
+app.get('/api/auth/github', async (req: Request, res: Response) => {
     const code = get(req, "query.code")
     const path = get(req, "query.path", "/")
     if(!code) {
         throw new Error("No Code");
     }
+    const githubUser = await getGithubUser({code})
+    console.log(githubUser)
+
+    res.redirect(path)
 })
 app.listen(4000, () => {
     console.log("Server is listening");
