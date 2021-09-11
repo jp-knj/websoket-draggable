cf0dd8443a269a4248eb354807ef9bc3dbdfb817
jp-knj <kenji.tomita1996@gmail.com>
2021-09-11 16:26:07 +0900

    :tada: Setup Github Link

--- lesson-o-auth/client/src/App.tsx
+++ lesson-o-auth/client/src/App.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+const GITHUB_CLIENT_ID = "f3f6b47cb120839f2e9c";
+const gitHubRedirectURL = "http://localhost:4000/api/auth/github";
+const path = "/";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <a
+          href={`https://github.com/login/oauth/authorize?client_id=${GITHUB_CLIENT_ID}&redirect_uri=${gitHubRedirectURL}?path=${path}&scope=user:email`}
+      >
+        LOGIN WITH GITHUB
+      </a>
     </div>
   );
 }
