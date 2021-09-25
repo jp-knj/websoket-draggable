9ef5e4bdd996093b00f7febbba8f953f3db07e51
jp-knj <kenji.tomita1996@gmail.com>
2021-09-25 17:00:46 +0900

    :tada: setup apollo/client

--- hackernews-react-apollo/src/index.js
+++ hackernews-react-apollo/src/index.js
@@ -1,13 +1,32 @@
+// import @apollo/client
+import {
+  ApolloProvider,
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache
+} from '@apollo/client';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
+// create httpLink for connecting apolloclient, graphql server will be running on 'http://localhost:4000'
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000'
+});
+
+// passing httpLink & InmemoryCache.
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+});
+
 ReactDOM.render(
-  <React.StrictMode>
+   <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root')
 );
 
