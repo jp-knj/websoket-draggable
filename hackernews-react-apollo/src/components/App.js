aff8700c3c87106a578913cb193eb19ed2294d88
jp-knj <kenji.tomita1996@gmail.com>
2021-09-26 13:39:57 +0900

    :tada: add search components

--- hackernews-react-apollo/src/components/App.js
+++ hackernews-react-apollo/src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Login from './Login';
+import Search from './Search'
 import CreateLink from './CreateLink';
 import Header from './Header';
 import LinkList from './LinkList';
@@ -18,6 +19,7 @@ const App = () => {
             component={CreateLink}
           />
           <Route exact path="/login" component={Login} />
+          <Route exact path="/search" component={Search} />
         </Switch>
       </div>
     </div>
